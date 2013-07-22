0b3235be804ac7a864fd54478a33c3af23ce3e6e
karmazin <anton.karmazin@softline.ru>
2013-07-22 18:03:43 +0400

    удаление блокировки в списке

--- j/options.js
+++ j/options.js
@@ -1,7 +1,11 @@
 $(document).ready(function(){
 	var list = SB.getAllBlocked();
 	for (var key in list) {
-		$('#blocklist').append('<div>'+ key +' '+'</div>');
+		$('body').append('<div><span>'+ key +'</span><span>'+ '<input type="button" value="Разблокировать" class="remove" id="'+key+'">' +'</span></div>');
 	}
 
+	$('.remove').on('click', function() {
+		SB.removeBlock($(this).attr('id'));
+		$(this).parents('div').remove();
+	});
 });
