ead6b51d91e02713da536577bc9a7ddfef6ddca3
karmazin <anton.karmazin@softline.ru>
2013-12-11 17:04:59 +0400

    add bootstrap add image change code of options page

--- j/options.js
+++ j/options.js
@@ -1,11 +1,36 @@
 $(document).ready(function(){
-	var list = SB.getAllBlocked();
-	for (var key in list) {
-		$('body').append('<div><span>'+ key +'</span><span>'+ '<input type="button" value="Разблокировать" class="remove" id="'+key+'">' +'</span></div>');
+
+	var table = {
+		place: $('.blocked-list'),
+		addRow: function(key) {
+			table.place.append('<tr><td>'+ key +'</td><td>'+ '<input type="button" class="btn btn-danger remove-key_action" value="Разблокировать" id="'+key+'">' +'</td></tr>');
+		},
+		check: function() {
+			return !$.isEmptyObject(SB.getAllBlocked());
+		},
+		setEmpty: function() {
+			table.place.append('<tr><td colspan="2">Пока здесь пусто, заблокируй сайт и он появится в этом списке</td></tr>');
+		},
+		remove: function(row) {
+			row.parents('tr').remove();
+		}
 	}
 
-	$('.remove').on('click', function() {
+	var list = SB.getAllBlocked();
+	if (table.check()) {
+		for (var key in list) {
+			table.addRow(key);
+		}
+	} else {
+		table.setEmpty();
+	}
+
+	$('.remove-key_action').on('click', function() {
 		SB.removeBlock($(this).attr('id'));
-		$(this).parents('div').remove();
+		table.remove($(this));
+
+		if (!table.check()) {
+			table.setEmpty();
+		}
 	});
 });
