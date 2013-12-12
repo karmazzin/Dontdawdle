d16c9c383cc23418f7cf6feae55d75e8ff2e316b
karmazin <anton.karmazin@softline.ru>
2013-12-12 12:01:58 +0400

    add button to actions: redirect to options and quick unlock

--- j/blocked.js
+++ j/blocked.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
-	$('#blocked').html(SM.get('last_blocked'));
+	chrome.tabs.onUpdated.addListener(function(tabId, changedInfo, tab) {
+
+		$('#unlock_action').on('click', function() {
+			SB.removeBlock(SM.get('last_domain'));
+			chrome.tabs.update(tabId, {"url" : SM.get('last_domain')});
+		});
+
+		$('#to-list_action').on('click', function() {
+			chrome.tabs.update(tabId, {"url" : "options.html"});
+		});
+	});
 });
