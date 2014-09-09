81d08d832cbc7cf138eae39f506e7485fad9f170
karmazin <karmazzin@ya.ru>
2014-09-10 01:36:20 +0400

    Добавлена библиотека angularjs В popup создан angular-app (!ng-csp).

---
+++ js/app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+(function(){
+    var app = angular.module('dontdawdle', []);
+
+    app.controller('PopupController', function($scope, Chrome) {
+        var vm = this;
+
+        vm.url = '';
+
+
+        vm.lockCurrentUrl = function() {
+//            console.log();
+
+        };
+    });
+
+
+
+})();
